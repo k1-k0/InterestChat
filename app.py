5a76839cf77e0b03037a80e56147db771196e5dd
k1-k0 <kmssrvk@gmail.com>
2019-09-12 09:03:49 +0300

    Small code notes and refactoring. Apply Flask-SocketIO module

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, request, render_template, redirect, url_for, g
+
+from flask_socketio import SocketIO
+
 from user import User
 from room import Room
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'like_secret!'
+socketio = SocketIO(app)
+
+
 person = None
 rooms = [
     Room('love'),
@@ -51,12 +58,23 @@ def room(room_name):
         rooms_name = [room.name for room in rooms]
         if room_name in rooms_name:
             print('User {} has been joined to "{}" room'.format(person.username, room.name))
+            
+            #TODO: Need send message about user joined to room
+            
             return render_template('room.html', room_name=room_name)
         else:
             return redirect(url_for('lobby'))
     else:
-        if request.form['message']:
-            room.messages.append(request.form['message'])
+        text_message = request.form['message']
+        if text_message:
+            print('User {username} in the "{room}" room say: "{message}"'.format(room=room_name,
+                                                                                 username=person.username,
+                                                                                 message=text_message))
+            room.messages.append(text_message)
             return render_template("room.html", room_name=room.name)
         else:
             print("STUB")
+
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
