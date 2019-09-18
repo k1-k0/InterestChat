37f340419211494121f229e28cf11f43ca45aaee
k1-k0 <kmssrvk@gmail.com>
2019-09-18 10:22:08 +0300

    Implemented JSON conversions methods for user

--- user.py
+++ user.py
@@ -1,13 +1,28 @@
 import uuid
+import json
 
 class User:
-    def __init__(self, username):
-        self.id = uuid.uuid4()
+    def __init__(self, username, room=None, id=uuid.uuid4().hex):
         self.username = username
-        self.room = None
+        self.room = room
+        self.id = id
 
     def connect(self, room_id):
         self.room = room_id
 
     def disconnect(self):
         self.room = None
+
+    def toJSON(self):
+        return json.dumps({
+            'id': self.id,
+            'username': self.username,
+            'room': self.room
+        })
+
+    @staticmethod
+    def fromJSON(user_json): 
+        user = json.loads(user_json)
+        return User(user['username'],
+                    user['room'],
+                    user['id'])
